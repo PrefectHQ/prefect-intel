7f1cc776070468fe0eb9c23312cbd728f557cb2b
Michael Adkins <michael@prefect.io>
2022-05-18 13:25:55 -0500

    Cleanup prints in demo

--- examples/packaging-demo.py
+++ examples/packaging-demo.py
@@ -1,5 +1,4 @@
 from prefect_intel.packaging import package_function, unpackage_function
-from pprint import pprint
 
 
 def add(x, y):
@@ -8,11 +7,14 @@ def add(x, y):
 
 if __name__ == "__main__":
     packaged = package_function(add, pickle_protocol="cloudpickle")
-    pprint(packaged)
+    print(f"Pickle package: {packaged!r}")
     unpackaged = unpackage_function(packaged)
     assert unpackaged(1, 2) == 3
 
+    print()
+
     packaged = package_function(add)
+    print(f"Script package: {packaged!r}")
     assert not packaged.pickle_protocol
     unpackaged = unpackage_function(packaged)
     assert unpackaged(1, 2) == 3
