f476562ce0b55fa5c74321c8cf05104b901ca405
Nathan Nowack <thrast36@gmail.com>
2022-06-22 12:14:53 -0500

    init

---
+++ examples/kubernetes-flow-runner.py
@@ -0,0 +1,19 @@
+from prefect import flow
+from datetime import datetime
+from prefect.deployments import DeploymentSpec
+from prefect.flow_runners import KubernetesFlowRunner
+
+@flow
+def what_day_is_it(date: datetime = None):
+    if date is None:
+        date = datetime.utcnow()
+    print(f"It was {date.strftime('%A')} on {date.isoformat()}")
+
+DeploymentSpec(
+    flow=what_day_is_it,
+    name="flow-deployment",
+    flow_runner=KubernetesFlowRunner(env={"MY_VARIABLE": "FOO"})
+)
+
+if __name__ == "__main__":
+    what_day_is_it("2021-01-01T02:00:19.180906")
