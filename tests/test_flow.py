b815b50fc54155bb706ea5f19894a3699970e6a8
sahiler <srangwala1998@gmail.com>
2022-07-28 12:18:34 -0500

    retesting 2.0 flow with new name

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -3,7 +3,7 @@ import pytest
 from prefect.testing.utilities import prefect_test_harness
 
 @pytest.fixture(autouse=True, scope="session")
-def prefect_test_fixture():
+def test_fixture():
     with prefect_test_harness():
         yield
 
