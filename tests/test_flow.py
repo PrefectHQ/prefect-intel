9f981ca1166ea314a022ad022e49ac48cb9d07f3
sahiler <srangwala1998@gmail.com>
2022-07-28 11:52:34 -0500

    retesting 2.0 flow

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,16 +1,15 @@
 from prefect import flow
-from prefect.testing.utilities import prefect_test_harness
 import pytest
+from prefect.testing.utilities import prefect_test_harness
 
+@pytest.fixture(autouse=True, scope="session")
+def prefect_test_fixture():
+    with prefect_test_harness():
+        yield
 
 @flow
 def my_favorite_flow():
     return 42
 
-@pytest.fixture(autouse=True, scope="session")
 def test_my_favorite_flow():
-  with prefect_test_harness():
-      # run the flow against a temporary testing database
-      assert my_favorite_flow() == 42 
-
-
+    assert my_favorite_flow() == 42
